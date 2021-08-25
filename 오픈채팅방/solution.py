47533f477f90dc6a4ae935855d9d699042e31b89
jjo <k01066624566@gmail.com>
2021-08-26 08:05:10 +0900

    sole 오픈채팅방 with python

---
+++ 오픈채팅방/solution.py
@@ -0,0 +1,14 @@
+def solution(record):
+    answer = []
+    user={}
+    for i in range(len(record)):
+        act=record[i].split(' ')
+        if(act[0]!="Leave"):
+            user[act[1]]=act[2]
+    for i in range(len(record)):
+        act=record[i].split(' ')
+        if act[0]=="Enter":
+            answer.append(user[act[1]]+"님이 들어왔습니다.")
+        elif act[0]=="Leave":
+            answer.append(user[act[1]]+"님이 나갔습니다.")
+    return answer
