3b088cff98231e2a393856f67bfc2969d51d0d5c
jjo <k01066624566@gmail.com>
2021-09-04 17:08:44 +0900

    tlqkf why don't solve this with js

---
+++ 모의고사/solution.js
@@ -0,0 +1,23 @@
+function solution(answers) {
+    var answer = [];
+    var mfp=[[1,2,3,4,5],[2,1,2,3,2,4,2,5],[3,3,1,1,2,2,4,4,5,5]];//mathforgiveperson
+    var max=-1;
+    var mfp_bingo=[0,0,0];
+    for(var i=0;i<mfp.length;i++){
+        for(var j=0;j<answers.length;j++){
+            var index=j>mfp[i].length-1?j-mfp[i].length:j;
+            if(answers[j]==mfp[i][index]){
+                mfp_bingo[i]++;
+            }
+        }
+        if(max<mfp_bingo[i]){
+          max=mfp_bingo[i];
+        }
+    }
+    for(var i=0;i<3;i++){
+      if(max==mfp_bingo[i]){
+        answer.push(i+1);
+      }
+    }
+    return answer;
+}
