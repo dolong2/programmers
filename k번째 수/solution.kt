71a97207019b705d547ed6670bde607eb375208d
jeayoung <k01066624566@gmail.com>
2022-12-05 12:06:05 +0900

    solve k번째 수 with kt

---
+++ k번째 수/solution.kt
@@ -0,0 +1,10 @@
+class Solution {
+    fun solution(array: IntArray, commands: Array<IntArray>): IntArray {
+        var answer = intArrayOf()
+        var temp = intArrayOf()
+        for(i in commands){
+            temp = array
+        }
+        return answer
+    }
+}
