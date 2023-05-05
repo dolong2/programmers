4441c4ea5eaa94ef5e49e56b190484543271fb57
조재영 <80161826+dolong2@users.noreply.github.com>
2023-05-05 14:37:17 +0900

    solve 중성화 여부 파악하기 with sql

---
+++ 중성화 여부 파악하기/solution.sql
@@ -0,0 +1,7 @@
+select ANIMAL_ID, NAME,
+    case
+        when sex_upon_intake like '%Neutered%' or sex_upon_intake like '%Spayed%'
+            then 'O'
+        else 'X'
+        end as '중성화'
+from animal_ins order by animal_id;
